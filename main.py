ebea2868bb7054383823ae0513fc83522db53cf6
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-05-29 21:48:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from typing import Union
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from database import models
+from database.database import engine
+from routes import file_upload
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+origin = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origin,
+    allow_credentials=True,
+    allow_methods=["POST", "GET", "PUT", "DELETE", "PATCH"],
+    allow_headers=["Authorization" "content-type", "Access-Control-Allow-Origin"],
+)
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(file_upload.router)
