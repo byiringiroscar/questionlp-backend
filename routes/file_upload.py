a54b5226fcffc81eab04c25e2befa9c614739ef1
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-05-29 22:27:17 +0200

    Update file_upload.py

--- routes/file_upload.py
+++ routes/file_upload.py
@@ -9,14 +9,16 @@ from random import randint
 from sqlalchemy.sql.expression import text
 import random
 import string
+import json
 
 
 
 router = APIRouter(prefix='/question', tags=['QuestionNLP'])
 
 
-@router.post("/file_upload", response_model=FileUpload)
-async def file_upload_display(file_upload: FileUpload, file: UploadFile, db: Session=Depends(get_db)):
+@router.post("/file_upload")
+async def file_upload_display(file: UploadFile, db: Session=Depends(get_db)):
+    data = json.loads(file.file.read())
     # file_name = file_upload.file_name
     # file_content = file_upload.file_content
     # db_file = models.FileUpload(file_name=file_name, file_content=file_content)
@@ -25,7 +27,9 @@ async def file_upload_display(file_upload: FileUpload, file: UploadFile, db: Ses
     # db.refresh(db_file)
 
     return {
-        'success': 'file_uploaded'
+        'success': 'file_uploaded',
+        'content': data,
+        'file_name': file.filename
     }
 
 
