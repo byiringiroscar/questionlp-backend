ba9923b6c16a03c510e6704bdec37d08b4f10a52
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-05-31 10:34:41 +0200

    Update langchainsetup.py

--- utils/langchainsetup.py
+++ utils/langchainsetup.py
@@ -22,3 +22,16 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text+= page.extract_text()
     return  text
+
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vector_store(text_chunks, folder_id):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    # save to folder faiss_index/folder_id
+    vector_store.save_local(f"faiss_index/{folder_id}")
