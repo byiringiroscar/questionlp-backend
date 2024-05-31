3a79877ee3d22dae095ce5d020408443b59dbb65
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-05-31 13:18:27 +0200

    Update file_upload.py

--- routes/file_upload.py
+++ routes/file_upload.py
@@ -81,9 +81,10 @@ async def ask_question(question: str, folder_id: Optional[int] = None , db: Sess
     response = user_input(question, folder_document_id)
     # save to database 
     new_question = models.QuestionAnswer(question=question, answer=response, fileupload_id=folder_document_id)
-    return {
-        "response": response
-    }
+    db.add(new_question)
+    db.commit()
+    db.refresh(new_question)
+    return JSONResponse(content={"response": response}, status_code=status.HTTP_200_OK)
     
 
 
