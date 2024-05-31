12ca237f376554d4125674323a98d6e1760eafc4
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-05-31 13:13:28 +0200

    Add models for question and answer

--- database/models.py
+++ database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
@@ -13,3 +13,16 @@ class FileUpload(Base):
     file_name = Column(String, nullable=False)
     file_content = Column(String, nullable=False)
     time_uploaded = Column(TIMESTAMP, default=datetime.utcnow())
+    questionanswers = relationship("QuestionAnswer", back_populates="fileupload")
+
+
+
+class QuestionAnswer(Base):
+    __tablename__ = "questionanswers"
+    id = Column(Integer, primary_key=True, nullable=False)
+    fileupload_id = Column(Integer, ForeignKey("fileuploads.id"))
+    question = Column(String, nullable=False)
+    answer = Column(String, nullable=False)
+    time_uploaded = Column(TIMESTAMP, default=datetime.utcnow())
+
+    fileupload = relationship("FileUpload", back_populates="questionanswers")
